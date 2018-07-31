ec658c656f556f43ded71be03beb1bab302bf8b6
Rahul Goutham <rahul.banka@navaratan.com>
2018-07-31 16:11:05 +0530

    Support multiple domains and large apps with efficiency

--- lib/letsencrypt-rails-heroku/letsencrypt.rb
+++ lib/letsencrypt-rails-heroku/letsencrypt.rb
@@ -8,18 +8,22 @@ module Letsencrypt
     yield(configuration) if block_given?
   end
 
-  def self.challenge_configured?
-    configuration.acme_challenge_filename && 
-      configuration.acme_challenge_filename.start_with?(".well-known/") &&
-      configuration.acme_challenge_file_content
+  def self.challenge_configured?(acme_challenge_filename)
+    puts acme_challenge_filename.inspect
+    puts configuration.acme_challenges.class
+    puts configuration.acme_challenges.inspect
+    puts configuration.acme_challenges.is_a?(Hash)
+    puts configuration.acme_challenges.key?(acme_challenge_filename)
+    configuration.acme_challenges.is_a?(Hash) &&
+      configuration.acme_challenges.key?(acme_challenge_filename)
   end
 
   class Configuration
     attr_accessor :heroku_token, :heroku_app, :acme_email, :acme_domain,
       :acme_endpoint, :ssl_type
-    
+
     # Not settable by user; part of the gem's behaviour.
-    attr_reader :acme_challenge_filename, :acme_challenge_file_content
+    attr_reader :acme_challenges
 
     def initialize
       @heroku_token = ENV["HEROKU_TOKEN"]
@@ -28,8 +32,7 @@ module Letsencrypt
       @acme_domain = ENV["ACME_DOMAIN"]
       @acme_endpoint = ENV["ACME_ENDPOINT"] || 'https://acme-v01.api.letsencrypt.org/'
       @ssl_type = ENV["SSL_TYPE"] || 'sni'
-      @acme_challenge_filename = ENV["ACME_CHALLENGE_FILENAME"]
-      @acme_challenge_file_content = ENV["ACME_CHALLENGE_FILE_CONTENT"]
+      @acme_challenges = ENV["ACME_CHALLENGES"] ? JSON.parse(ENV["ACME_CHALLENGES"]) : nil
     end
 
     def valid?
